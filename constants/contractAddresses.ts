ebb0380d8802b888ccb6671cfe538ccc3a3cddcc
bd rex <damilolabamidele53@gmail.com>
2023-08-12 16:47:53 +0100

    first commit

---
+++ constants/contractAddresses.ts
@@ -0,0 +1,59 @@
+export const nftDropContractAddress = "0x1720887ebfeB15B0f611154d561218c49c27295c";
+export const tokenContractAddress = "0xbA0250a138a365327fd4177D0Ecb6287E1bC3Ca7";
+export const stakingContractAddress = "0x3297b5999c355ee21ce71e21BB0A5D1632244323";
+//0x36a9848B9f4669E8aF13841FCD8f6AA4FC0b91bb
+//0xe550Eb934C2291D9A4C9913336409117936f80A0
+
+
+
+
+
+
+// // amount
+// : 
+// "1"
+// block_number
+// : 
+// "42349027"
+// block_number_minted
+// : 
+// "42349027"
+// contract_type
+// : 
+// "ERC721"
+// last_metadata_sync
+// : 
+// "2023-05-06T14:36:31.422Z"
+// last_token_uri_sync
+// : 
+// "2023-05-05T21:17:59.751Z"
+// metadata
+// : 
+// "{\"name\":\"Flowers On A Wednesday #B6\",\"description\":\"Flowers and Tigers and Bears, FlowerPets, Creepets and SteamPets.\",\"image\":\"ipfs://QmNnf2TqLb73ChDyFrr5Vka766dT9DXaZ5RRn8yTg2LfuN/6.png\",\"attributes\":[],\"compiler\":\"mintfoundry.xyz\"}"
+// minter_address
+// : 
+// null
+// name
+// : 
+// "GREAT FLOWERS"
+// owner_of
+// : 
+// "0x80cd52607b7ebd0f9953989cfab6b2d74e9ddee7"
+// possible_spam
+// : 
+// false
+// symbol
+// : 
+// "GDAFOAW"
+// token_address
+// : 
+// "0x421ef0d9679d1c24ef976b06638566a64aac2780"
+// token_hash
+// : 
+// "1ef33fbe357f5a2300bb2452cc68175c"
+// token_id
+// : 
+// "6"
+// token_uri
+// : 
+// "
